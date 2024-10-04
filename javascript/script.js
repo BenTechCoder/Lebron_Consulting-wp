578be8be21d2b8f69fc12659e3fef0707858ea39
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2024-10-03 22:20:02 -0400

    added reel JS to correct file

--- javascript/script.js
+++ javascript/script.js
@@ -8,3 +8,27 @@
  * For esbuild documentation, please see:
  * https://esbuild.github.io/
  */
+
+// REEL from every layout
+
+(function() {
+    const className = 'reel';
+    const reels = Array.from(document.querySelectorAll(`.${className}`));
+    const toggleOverflowClass = elem => {
+      elem.classList.toggle('overflowing', elem.scrollWidth > elem.clientWidth);
+    };
+  
+    for (let reel of reels) {
+      if ('ResizeObserver' in window) {
+        new ResizeObserver(entries => {
+          toggleOverflowClass(entries[0].target);
+        }).observe(reel);
+      }
+  
+      if ('MutationObserver' in window) {
+        new MutationObserver(entries => {
+          toggleOverflowClass(entries[0].target);
+        }).observe(reel, {childList: true});
+      }
+    }
+  })();
