d416e19e99341e19368c6b2e7105d0eeb8500ff0
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2024-10-09 13:18:50 -0400

    contact component update (switch of contact form 7)

--- theme/template-parts/components/contact-form.php
+++ theme/template-parts/components/contact-form.php
@@ -6,8 +6,32 @@
 		</div>
    
 	<div class="center text-left"> 
-		<?php echo do_shortcode( '[contact-form-7 id="96b13aa" title="Contact form 1"]' ); ?>
+		<form action="https://formsubmit.co/ben@lebronconsulting.tech" method="POST">
+			<div class="flow">
+				<div class="stack">
+					<label for="name">Name</label>
+					<input type="text" name="name" id="name" maxlength="50" required>
+				</div>
+				<div class="stack">
+					<label for="name">Email</label>
+					<input type="email" name="email" id="email">
+				</div>
+				<div class="stack">
+					<label for="phone">Phone Number</label>
+					<input type="tel" name="phone number" id="phone">
+				</div>
+			</div>
+			<div class="stack">
+				<label for="message">Message</label>
+				<textarea name="message" id="message" cols="30" rows="10"></textarea>
+			</div>
+			<button type="submit" class="btn">Submit</button>
+		</form>
 	</div>
 		<!-- TODO: ADD EMAIL AND PHONE NUMBER -->
+			<ul class="center flow">
+				<li><a href="mailto:contact@lebronconsulting.tech">contact@lebronconsulting.tech</a></li>
+				<li><a href="tel:9197061910">(919)-706-1910</a></li>
+			</ul>
 </div>
 </section>
