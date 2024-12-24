7498a98aec93797ff7dd012cd7bdf244eb9588f4
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2024-12-24 16:38:13 -0500

    fix hero centering on large screens issue

--- theme/template-parts/components/hero.php
+++ theme/template-parts/components/hero.php
@@ -3,7 +3,7 @@
     <!-- class="bg-cover bg-[url('http://lc-wp.local/wp-content/uploads/2024/08/Designer.png')]" -->
 <div class="pt-10 bg-primary/50 text-light">
        <div class="hero">
-           <div class="hero__inner text-center">
+           <div class="hero__inner text-center center">
                 <p>Good Websites for your small business</p>
                 <h1>WEBSITES THAT WORK AS HARD AS YOU DO</h1>
            </div>
