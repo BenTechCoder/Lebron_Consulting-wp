e33f67fbccdc76fce44a59ee44defd331ab4985f
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2025-03-26 09:43:47 -0400

    switch to smtp and contact form 7

--- theme/template-parts/components/contact-form.php
+++ theme/template-parts/components/contact-form.php
@@ -1,37 +1,17 @@
 <section class="contact | bg-primary text-light py-10">
-<div class="wrapper stack">
+	<div class="wrapper stack">
 		<div class="center stack">
 			<h2>Add a new tool to your business</h2>
 			<p>Send us a message, we're always ready to make awesome websites.</p>
 		</div>
-   
-	<div class="center text-left"> 
-		<form action="https://formsubmit.co/a56bbc80f8a25e9573b084e94488a8ff" method="POST">
-			<div class="flow">
-				<div class="stack">
-					<label for="name">Name</label>
-					<input type="text" name="name" id="name" maxlength="50" required>
-				</div>
-				<div class="stack">
-					<label for="name">Email</label>
-					<input type="email" name="email" id="email">
-				</div>
-				<div class="stack">
-					<label for="phone">Phone Number</label>
-					<input type="tel" name="phone number" id="phone">
-				</div>
-			</div>
-			<div class="stack">
-				<label for="message">Message</label>
-				<textarea name="message" id="message" cols="30" rows="10"></textarea>
-			</div>
-			<button type="submit" class="btn">Submit</button>
-		</form>
-	</div>
+
+		<div class="center text-left">
+			<?php echo do_shortcode('[contact-form-7 id="96b13aa" title="Contact form 1"]'); ?>
+		</div>
 		<!-- TODO: ADD EMAIL AND PHONE NUMBER -->
-			<ul class="center flow">
-				<li><a href="mailto:contact@lebronconsulting.tech">contact@lebronconsulting.tech</a></li>
-				<li><a href="tel:9197061910">(919)-706-1910</a></li>
-			</ul>
-</div>
+		<ul class="center flow">
+			<li><a href="mailto:contact@lebronconsulting.tech">contact@lebronconsulting.tech</a></li>
+			<li><a href="tel:9197061910">(919)-706-1910</a></li>
+		</ul>
+	</div>
 </section>
